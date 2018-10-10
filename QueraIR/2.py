a4dfbf7a178d4cbd607aa5b7a19fc27b5acf4790
Ali Movahedi <ali_movahedi@aol.com>
2018-10-10 10:43:48 +0330

    make solution better

--- QueraIR/2.py
+++ QueraIR/2.py
@@ -1,13 +1,8 @@
 max = 0
 
 for i in range(0, int(input())):
-    current = input()
-
-    if len(current) < max:
-        continue;
-
-    uniqueness = set(current)
-    if len(uniqueness) > max:
-        max = len(uniqueness)
+    current = set(input())
+    if len(current) > max:
+        max = len(current)
 
 print(max)
