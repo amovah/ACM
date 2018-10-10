049e267ddc1c2f1b5fd445b87e9435930a4e8b19
Ali Movahedi <ali_movahedi@aol.com>
2018-10-10 11:38:57 +0330

    solution for question 3

--- QueraIR/3.py
+++ QueraIR/3.py
@@ -1,3 +1,26 @@
-inp = input()
+stock = [int(item) for item in input().split(' ')]
 
-print(inp)
+for i in range(len(stock)):
+    stock[i] = stock[i] % 2
+
+while stock.count(1) > 1:
+    counter = 0
+    selected = -1
+
+    for i in range(len(stock)):
+        if stock[i] == 1 and counter < 2:
+            stock[i] = 0
+            counter = counter + 1
+        elif selected == -1:
+            selected = i
+
+    stock[selected] = (stock[selected] + 1) % 2
+
+result = []
+for item in stock:
+    if item:
+        result.append('YES')
+    else:
+        result.append('NO')
+
+print(' '.join(result))
