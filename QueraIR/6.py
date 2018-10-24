a5086c68dd159f0b2ede93e9153a90d8120532c0
Ali Movahedi <ali_movahedi@aol.com>
2018-10-24 10:47:31 +0330

    remove useless importing lib

--- QueraIR/6.py
+++ QueraIR/6.py
@@ -1,7 +1,5 @@
 # problem: http://www.algorithmha.ir/%D9%85%D8%B3%D8%A6%D9%84%D9%87-%D8%A7%D9%84%DA%AF%D9%88%D8%B1%DB%8C%D8%AA%D9%85%DB%8C/%D9%85%D8%B3%D8%A7%D9%84%D9%87-%D8%AD%D8%AF%D8%B3-%DA%A9%D9%88%D9%84%D8%A7%D8%AA%D8%B2/
 # I write it for my friend to learn much more
-import math
-
 def seq(start):
     answer = 0
     if start % 2:
