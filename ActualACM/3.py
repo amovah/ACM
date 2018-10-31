44e2961a39e04d23fd67890d6496406755dcfa42
Ali Movahedi <ali_movahedi@aol.com>
2018-10-31 18:41:04 +0330

    fix

--- ActualACM/3.py
+++ ActualACM/3.py
@@ -2,39 +2,40 @@
 # solution: it's mine.
 
 keypad = [
-    ['A', 'B', 'C'],
-    ['D', 'E', 'F'],
-    ['G', 'H', 'I'],
-    ['J', 'K', 'L'],
-    ['M', 'N', 'O'],
-    ['P', 'Q', 'R', 'S'],
-    ['T', 'U', 'V'],
-    ['W', 'X', 'Y', 'Z']
+['A', 'B', 'C'],
+['D', 'E', 'F'],
+['G', 'H', 'I'],
+['J', 'K', 'L'],
+['M', 'N', 'O'],
+['P', 'Q', 'R', 'S'],
+['T', 'U', 'V'],
+['W', 'X', 'Y', 'Z']
 ]
 
 def decrypt(letter, i):
-    status = letter.isupper()
-    if status is False:
-        letter = letter.upper()
+	status = letter.isupper()
+	if status is False:
+		letter = letter.upper()
 
-    for letters in keypad:
-        if letter in letters:
-            count = letters.index(letter)
-            if status is False:
-                return letters[(count - i - 1) % len(letters)].lower()
-            return letters[(count - i - 1) % len(letters)]
+	for letters in keypad:
+		if letter in letters:
+			count = letters.index(letter)
+			if status is False:
+				return letters[(count - i - 1) % len(letters)].lower()
+			return letters[(count - i - 1) % len(letters)]
 
 results = []
 while True:
-    voroodi = input()
-    if voroodi == '#':
-        break
+	voroodi = input()
+	if voroodi == '#':
+		break
 
-    result = []
-    for i in range(len(voroodi)):
-        result.append(decrypt(voroodi[i], i))
-    results.append(''.join(result))
+	result = []
+	for i in range(len(voroodi)):
+		result.append(decrypt(voroodi[i], i))
+
+	results.append(''.join(result))
 
 
 for x in results:
-    print(x)
+	print(x)
