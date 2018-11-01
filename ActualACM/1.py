431c97d877031583531402993992b0192fd4eb7e
Ali Movahedi <ali_movahedi@aol.com>
2018-11-01 15:52:46 +0330

    lint again

--- ActualACM/1.py
+++ ActualACM/1.py
@@ -4,53 +4,52 @@
 # and it's mine
 
 def answer(first, second, totalSum):
-    first = [int(x) for x in str(first)]
-    first.reverse()
-    second = [int(x) for x in str(second)]
-    second.reverse()
-    totalSum = [int(x) for x in str(totalSum)]
-    totalSum.reverse()
+	first = [int(x) for x in first]
+	first.reverse()
+	second = [int(x) for x in second]
+	second.reverse()
+	totalSum = [int(x) for x in totalSum]
+	totalSum.reverse()
 
-    results = []
+	results = []
 
-    firstColumn = 0
-    while True:
-        result = columnAddition(first, second, totalSum, firstColumn)
-        results.append(result['count'])
-        if result['usedColumn'] == -1:
-            break
-        else:
-            firstColumn = result['usedColumn'] + 1
+	firstColumn = 0
+	while True:
+		result = columnAddition(first, second, totalSum, firstColumn)
+		results.append(result['count'])
+		if result['usedColumn'] == -1:
+			break
+		else:
+			firstColumn = result['usedColumn'] + 1
 
-    return min(results)
+	return min(results)
 
 
 def columnAddition(first, second, totalSum, firstColumn = 0):
-    stack = 0
-    count = firstColumn
-    usedColumn = -1
-    for i in range(firstColumn, len(first)):
-        sum = str(first[i] + second[i])
-        if len(sum) == 1:
-            sum = '0' + sum
-        sum = [int(x) for x in sum]
-        sum.reverse()
+	stack = 0
+	count = firstColumn
+	usedColumn = -1
+	for i in range(firstColumn, len(first)):
+		sum = str(first[i] + second[i])
+		if len(sum) == 1:
+			sum = '0' + sum
+		sum = [int(x) for x in sum]
+		sum.reverse()
+		if (sum[0] + stack) == totalSum[i]:
+			stack = sum[1]
+			if usedColumn == -1:
+				usedColumn = i
+		else:
+			count = count + 1
 
-        if (sum[0] + stack) == totalSum[i]:
-            stack = sum[1]
-            if usedColumn == -1:
-                usedColumn = i
-        else:
-            count = count + 1
-
-    return {
-        'count': count,
-        'usedColumn': usedColumn
-    }
+	return {
+		'count': count,
+		'usedColumn': usedColumn
+	}
 
 result = []
 while(input() != '0'):
-    result.append(answer(int(input()), int(input()), int(input())))
+	result.append(answer(input(), input(), input()))
 
 for x in result:
-    print(x)
+	print(x)
--- ActualACM/2.py
+++ ActualACM/2.py
@@ -1,28 +1,26 @@
 # problem: http://www.algorithmha.ir/%D9%85%D8%B3%D8%A6%D9%84%D9%87-%D8%A7%D9%84%DA%AF%D9%88%D8%B1%DB%8C%D8%AA%D9%85%DB%8C/%D9%85%D8%B3%D8%A7%D9%84%D9%87-%D8%AD%D8%AF%D8%B3-%DA%A9%D9%88%D9%84%D8%A7%D8%AA%D8%B2/
 # I write it for my friend to learn much more
 def seq(start):
-    answer = 0
-    if start % 2:
-        answer = start * 3 + 1
-    else:
-        answer = start // 2
+	answer = 0
+	if start % 2:
+		answer = start * 3 + 1
+	else:
+		answer = start // 2
 
-    if answer == 1:
-        return 2
+	if answer == 1:
+		return 2
 
-    return 1 + seq(answer)
+	return 1 + seq(answer)
 
 def answer(beginning, ending):
     # if beginning % 2 == 0:
     #     beginning = beginning + 1
 
-    result = []
-    for i in range(beginning, ending):
-        result.append(seq(i))
+	result = []
+	for i in range(beginning, ending):
+		result.append(seq(i))
 
-    return max(result)
+	return max(result)
 
-all = input().split(' ')
-first = int(input())
-second = int(input())
+first, second = [int(x) for x in input().split()]
 print(first, second, answer(first, second))
